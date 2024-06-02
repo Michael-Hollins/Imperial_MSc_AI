fd61243b6004832ab906b489d168811bb9288da3
Michael Hollins <mdh323@ic.ac.uk>
2024-06-02 22:22:36 +0100

    New volatility plot added for S3 in 2021

--- scripts/charts.py
+++ scripts/charts.py
@@ -7,7 +7,7 @@ from utilities import plot_remove_labels
 # Set parameters
 sns.set_style("whitegrid")
 data_path = 'C:/Users/micha/OneDrive/Documents/AI_MSc/thesis/Imperial_MSc_AI/data'
-figure_path = 'C:/Users/micha/OneDrive/Documents/AI_MSc/thesis/Imperial_MSc_AI/figures'
+figure_path = 'C:/Users/micha/OneDrive/Documents/AI_MSc/thesis/Imperial_MSc_AI/report/figures'
 plot_width_inch = 3   
 plot_height_inch = 2   
 dpi = 300
@@ -77,3 +77,31 @@ fig.savefig(os.path.join(figure_path, 'data_coverage.png'), dpi=dpi)
 plt.close()
 
 #############################################################
+# 2021 S3 data volatility
+# Source: Figure 9 https://www.lseg.com/content/dam/ftse-russell/en_us/documents/research/solving-scope-3-conundrum.pdf
+figures_2021 = [44, 21, 17, 18]
+categories = ['< 20%', '20% - 50%', '50% - 100%', '> 100%']
+colours = ['blue', 'lightblue', 'grey', 'darkblue']
+
+# Creating a horizontal bar plot with combined segments, annotations, and legend
+fig, ax = plt.subplots(figsize=(plot_width_inch, plot_height_inch), dpi=dpi)
+position = 0
+bars = []
+for value, color, label in zip(figures_2021, colours, categories):
+    bars.append(plt.barh(0, value, left=position, color=color, edgecolor='black', label=label))
+    # Adding text annotations in the middle of each segment
+    plt.text(position + value / 2, 0, f'{value}%', va='center', ha='center', color='white', fontsize=12, fontweight='bold')
+    position += value
+
+for spine in ax.spines.values():
+    spine.set_visible(False)
+    
+# Set labels and title
+fig.tight_layout()
+plt.xlim(0, 100)
+plot_remove_labels(ax)
+plt.xticks([])
+plt.yticks([])
+plt.legend(loc='center', bbox_to_anchor=(0.5, -0.15), ncol=2, frameon=False, fontsize='xx-small')
+fig.savefig(os.path.join(figure_path, 's3_2021_vol.png'), dpi=dpi)
+plt.close()
