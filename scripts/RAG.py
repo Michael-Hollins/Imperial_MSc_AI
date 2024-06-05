6d3e797bba209e63f9fcc87cacea9da0405a92d7
Michael Hollins <mdh323@ic.ac.uk>
2024-06-05 17:56:26 +0100

    Updates to the directory

--- scripts/RAG.py
+++ scripts/RAG.py
@@ -1,8 +1,18 @@
-from langchain_community.llms import Ollama
-from langchain_community.embeddings import OllamaEmbeddings
+import os
+import openai
+import sys
+sys.path.append('../..')
 
-MODEL = "tinyllama"
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
 
-model = Ollama(model=MODEL)
-embeddings = OllamaEmbeddings(model=MODEL)
-model.invoke("what is machine learning in a few words?")
+openai.api_key  = os.environ['OPENAI_API_KEY']
+
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+persist_directory = 'vector_db/chroma/'
+embedding = OpenAIEmbeddings()
+vectordb = Chroma(persist_directory=persist_directory, embedding_function=embedding)
+
+print(vectordb._collection.count())
+
